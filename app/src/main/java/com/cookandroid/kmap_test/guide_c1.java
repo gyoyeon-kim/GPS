adb958e4cdfda148bc47d0a4c464c881a89e9295
gyoyeon Kim <gy2121@naver.com>
2023-05-21 13:27:18 +0900

    new_kamap 마커 등록 완료

---
+++ app/src/main/java/com/cookandroid/kmap_test/guide_c1.java
@@ -0,0 +1,15 @@
+package com.cookandroid.kmap_test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class guide_c1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guide_c1);
+    }
+
+}
---
+++ app/src/main/java/com/cookandroid/kmap_test/guide_c6.java
@@ -0,0 +1,15 @@
+package com.cookandroid.kmap_test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class guide_c6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.guide_c6);
+    }
+
+}
