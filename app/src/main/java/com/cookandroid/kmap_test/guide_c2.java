482bd6851859d98cdd3d78873644a9f08fd4a84a
gyoyeon Kim <gy2121@naver.com>
2023-05-29 00:44:28 +0900

    add guide building_abcd

--- app/src/main/java/com/cookandroid/kmap_test/guide_c2.java
+++ app/src/main/java/com/cookandroid/kmap_test/guide_c2.java
@@ -1,15 +1,217 @@
 package com.cookandroid.kmap_test;
 
-import android.os.Bundle;
-
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.Toast;
+import android.widget.ViewFlipper;
+
+import net.daum.mf.map.api.MapView;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class guide_c2 extends AppCompatActivity {
+    private ScrollView mScrollView;
+    private Button mbutton1,navi;
+
+    String user_latitude = "latitude";
+    String user_longitude= "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guide_c2);
+
+        Intent intent = getIntent();
+        double currentLatitude = getIntent().getDoubleExtra("current_latitude", 0.0);
+        double currentLongitude = getIntent().getDoubleExtra("current_longitude", 0.0);
+
+        mScrollView = findViewById(R.id.Scroll_View);
+        mbutton1 = findViewById(R.id.Button_1);
+        navi = findViewById(R.id.navi);
+
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        final GridView gridView = (GridView)findViewById(R.id.gridView1);
+        MyGridAdapter gridAdapter = new MyGridAdapter(this);
+        gridView.setAdapter(gridAdapter);
+
+
+        mbutton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mScrollView.fullScroll(ScrollView.FOCUS_UP);
+
+            }
+        });
+
+        navi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if ( Build.VERSION.SDK_INT >= 23 &&
+                        ContextCompat.checkSelfPermission( getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+                    ActivityCompat.requestPermissions(guide_c2.this, new String[] {
+                            android.Manifest.permission.ACCESS_FINE_LOCATION}, 0 );
+                }
+                else {
+                    // 가장최근 위치정보 가져오기
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (location != null) {
+                        String provider = location.getProvider();
+                        double longitude = location.getLongitude();
+                        double latitude = location.getLatitude();
+                        double altitude = location.getAltitude();
+                        user_latitude = String.valueOf(latitude);
+                        user_longitude = String.valueOf(longitude);
+                    }
+                    // 위치정보를 원하는 시간, 거리마다 갱신해준다.
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                            1000,
+                            1,
+                            gpsLocationListener);
+                }
+
+                String url2 = "kakaomap://route?sp=" + currentLatitude + "," + currentLongitude + "&ep=35.91320569912806, 128.81111918428113&by=FOOT";
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url2));
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                List<ResolveInfo> list = getPackageManager().queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+
+                if (list == null || list.isEmpty()) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=net.daum.android.map")));
+                } else {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+
+
+        // 권한ID를 가져옴
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+
+        int permission2 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int permission3 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        // 권한이 열려있는지 확인
+        if (permission == PackageManager.PERMISSION_DENIED || permission2 == PackageManager.PERMISSION_DENIED || permission3 == PackageManager.PERMISSION_DENIED) {
+            // 마쉬멜로우 이상버전부터 권한을 물어봄
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 권한 체크(READ_PHONE_STATE의 requestCode를 1000으로 세팅
+                requestPermissions(
+                        new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1000);
+            }
+            return;
+
+        }
+
+
+
     }
 
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            // 위치 리스너는 위치정보를 전달할 때 호출되므로 onLocationChanged()메소드 안에 위지청보를 처리를 작업을 구현 해야합니다.
+            String provider = location.getProvider();  // 위치정보
+            double longitude = location.getLongitude(); // 위도
+            double latitude = location.getLatitude(); // 경도
+            double altitude = location.getAltitude(); // 고도
+            user_latitude = String.valueOf(latitude);
+            user_longitude = String.valueOf(longitude);
+        } public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        } public void onProviderEnabled(String provider) {
+
+        } public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+
+
+
+    public class MyGridAdapter extends BaseAdapter{
+        Context context;
+        Integer[] posterId = {R.drawable.c2_1,R.drawable.c2_2,R.drawable.c2_3};
+        String[] mvName = {"이미지1","이미지2","이미지3"};
+
+        public MyGridAdapter(Context context){
+            this.context = context;
+        }
+
+        public int getCount(){
+            return posterId.length;
+        }
+
+        public Object getItem(int arg0){
+            return null;
+        }
+
+        public long getItemId(int arg0){
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new ViewGroup.LayoutParams(330, 200));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5,5,5,5);
+            imageView.setImageResource(posterId[position]);
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = (View)View.inflate(guide_c2.this,
+                            R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(guide_c2.this);
+                    ImageView ivPoster = (ImageView)dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterId[pos]);
+                    dlg.setTitle(mvName[pos]);
+                    dlg.setIcon(R.drawable.ic_launcher_foreground);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return imageView;
+        }
+    }
 }
